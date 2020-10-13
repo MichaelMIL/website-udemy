234eb2e067b124905e9ff03a05e032fbe45fa3d8
MichaelMIL <michaelmark16@gmail.com>
2020-10-13 13:38:06 +0300

    add TableView

---
+++ src/components/Common/TableView.js
@@ -0,0 +1,47 @@
+import React , {Component} from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+class TableView extends Component{
+    render(){
+        const {rows, columns} = this.props;
+        return(
+            <Paper>
+                <Table>
+                    <TableHead>
+                        {columns ?
+                            columns.map((col, i)=>{
+                                return(
+                                    <TableCell key={i}>{col.lable}</TableCell>
+                                )
+                            })
+                            
+                        : null}
+                    </TableHead>
+                    <TableBody>
+                        {rows ?
+                            rows.map((row, i)=>{
+                                return columns.map((col, colIndex)=>{
+                                    return(
+                                        <TableCell>
+                                            {row[col.name]}
+                                        </TableCell>
+                                )
+                                })
+                                
+                            })
+                        :null}
+                    </TableBody>
+                </Table>
+            </Paper>
+        )
+    }
+}
+
+
+export default TableView;
--- src/components/Pages/Admin/Users.js
+++ src/components/Pages/Admin/Users.js
@@ -1,9 +1,27 @@
 import React,{Component} from 'react';
+import TableView from '../../Common/TableView';
+import {connect} from 'react-redux';
+
+
+const columns =[
+    {lable: "ID", name:'id'},
+    {lable: "Email", name:'email'},
+    {lable: "Name", name:'name'}
+]
+
 
 class Users extends Component{
     render(){
         return(
-            <h1>User</h1>
+            <div>
+                <h1>User</h1>
+                <TableView 
+                    columns={columns}
+                    rows={[]}
+                />
+            </div>
+            
+            
         )
     }
 
