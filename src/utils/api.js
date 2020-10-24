2b387c601d009d285838e48e086c32c604896d64
MichaelMIL <michaelmark16@gmail.com>
2020-10-24 20:07:33 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ let host;
 if(process.env.NODE_ENV === 'development') {
     host = 'http://localhost:8080'
 } else{
-    host= 'http://api.example.com'
+    host= 'http://localhost'
 }
 
 const API= {
